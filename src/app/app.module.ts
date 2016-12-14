bd57033c6678e68993e79fed6faaef22e25fb06a
Josh Harwood <bocharwood@gmail.com>
2016-12-14 17:53:25 +0000

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DevicesComponent } from './devices/devices.component';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 import { MapComponent } from './map/map.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-
+// Define the routes
+const ROUTES = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DevicesComponent,
-    MapComponent
+    MapComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +31,8 @@ import { MapComponent } from './map/map.component';
     HttpModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBjvGgmTVf5gE0aLcivSDUSGmtDPmybj90'
-    })
+    }),
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
